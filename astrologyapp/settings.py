0874c0d65cc8df0b1f4d906e9939cff0e30f368f
Duarte Nunes <duartenunes4ever@gmail.com>
2023-11-03 21:34:09 +0000

    things

--- astrologyapp/settings.py
+++ astrologyapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&t!+$doeudmch$d)-gy016ddbjkxgv%&-eqq&&jh$vgp2&$3ig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
